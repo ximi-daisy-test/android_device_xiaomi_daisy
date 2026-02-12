72362cf94abea9f7dd4e3e201b8e7b9c1c5e09f8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-12 18:48:21 +0800

    daisy: lite -> Lite
    I had to reinstall stock rom to check something and yeah they name it like this

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_DEVICE := daisy
 PRODUCT_NAME := lineage_daisy
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi A2 lite
+PRODUCT_MODEL := Mi A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
